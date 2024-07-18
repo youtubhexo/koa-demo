bdfdf04b71b93a87ebae2ba16840324ec7725961
desperado <youtubhexo@gmail.com>
2024-07-18 05:57:08 -0700

    Requests属性、路由基础用法、app.use链式处理

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const Koa=require('koa');
+const Koa = require('koa');
+const Router = require('@koa/router');
 
-const app=new Koa();
+const app = new Koa();
+const router = new Router();
 
-app.use((ctx)=>{
-    ctx.body='Hello World';
+
+router.get('/', (ctx) => {
+    // ctx.router available
+    ctx.body = ('AI赋能开发')
 });
+
+router.get('/async', async(ctx) => {
+  const result = await new Promise((resolve) => {
+    setTimeout(() => {
+       resolve('AI赋能开发')
+    }, 5000)
+  })
+    ctx.body = result
+})
+   
+
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+//1 request  response
+//2 router
+//3 ctx,async
+
 module.exports = app;
