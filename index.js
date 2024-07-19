296f1842277a4a1fabb29dc38d2150789f412c02
desperado <youtubhexo@gmail.com>
2024-07-18 20:19:13 -0700

    koa开发RESTful接口

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Router = require('@koa/router');
 
 const app = new Koa();
 const router = new Router();
+const cors = require('@koa/cors');
+const { koaBody } = require('koa-body');
 
 
 router.get('/', (ctx) => {
@@ -18,12 +20,51 @@ router.get('/async', async(ctx) => {
   })
     ctx.body = result
 })
+router.post(
+  '/post/upload',
+  koaBody({
+    multipart: true,
+    formidable: {
+      uploadDir: __dirname + '/uploads',
+    },
+  }),
+  (ctx) => {
+    const fields = ctx.request.body.fields; // this will be undefined for file uploads
+    const files = ctx.request.files;
+    console.log('files', JSON.stringify(files, null, 2));
+    /*{
+      "requestFields": null,
+      "requestFiles": {
+        "source": {
+          "size": 748831,
+          "path": "/some-dir/upload_cc1e0c49b97af0b9ef17b7b2f96b307d",
+          "name": "avatar.png",
+          "type": "image/png",
+          "mtime": "2018-07-07T14:16:22.576Z"
+        }
+      }
+    }*/
+
+    // respond with the fields and files for example purposes
+    ctx.body = JSON.stringify(
+      {
+        requestFields: fields || null,
+        requestFiles: files || null,
+      },
+      null,
+      2,
+    );
+  },
+);
    
 
-
+app.use(cors({
+  allowMethods: ['GET', 'POST'],
+}));
 app
   .use(router.routes())
   .use(router.allowedMethods());
+
 
 //1 request  response
 //2 router
