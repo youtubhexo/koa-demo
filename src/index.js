ab4bb8b056fb533d85f350879f37699d4d8f60e3
desperado <youtubhexo@gmail.com>
2024-07-21 01:25:09 -0700

    静态资源服务

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const router = require('./routers/routers')
 const app = new Koa();
 const cors = require('@koa/cors');
 const { koaBody } = require('koa-body');
+const serve = require('koa-static');
+const path=require('path')
 
 
    
@@ -13,6 +15,7 @@ app.use(cors({
 }));
 app
 .use(router())
+app.use(serve(path.join(__dirname, '../public')));
 
 
 //1 request  response
