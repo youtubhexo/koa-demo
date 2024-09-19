ed5c259538179276b05f819f3325f62ccbb09f25
desperado <youtubhexo@gmail.com>
2024-09-20 07:09:23 +0800

    koa-router介绍

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
 const app = new Koa();
+const router = new Router();
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+//  request   response
+//  router  ->根据不同的路由，跳转到不同的方法
+router.get('/', (ctx, next) => {
+  // ctx.router available
+  ctx.body='AI赋能开发'
 });
 
+router.get('/about', (ctx, next) => {
+  // ctx.router available
+  ctx.body='这是关于页面的api数据'
+});
+app
+.use(router.routes())
+.use(router.allowedMethods());
 app.listen(3000);
